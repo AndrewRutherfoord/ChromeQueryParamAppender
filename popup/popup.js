1b36a3154e56c92af9a87348891c46d4895ae8db
Andrew Rutherfoord <andrew.rutherfoord@live.com>
2023-04-24 19:41:56 +0200

    Added delete button to table

--- popup/popup.js
+++ popup/popup.js
@@ -6,63 +6,67 @@ import {
   getUrl,
 } from "../utils.js";
 
+// Renders the host params table.
 function renderTable() {
-  let paramsListHtml = "";
-  Object.entries(paramsData).forEach(([key, value]) => {
-    paramsListHtml += `<tr><th scope="row">${key}</th><td>${value}</td></tr>`;
-  });
-
   if (Object.keys(queryParams).length > 0) {
-    paramsTable.innerHTML = `
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Key</th>
-            <th scope="col">Value</th>
-          </tr>
-        </thead>
-          <tbody id="paramsTableBody">
-            ${paramsListHtml}
-          </tbody>
-      </table>
-    `;
+    tableBody.innerHTML = "";
+    Object.entries(queryParams).forEach(([key, value]) => {
+      tableBody.innerHTML += `<tr><th scope="row">${key}</th><td>${value}</td><td><button id="delete-button" data-key="${key}" class="btn btn-danger btn-sm">Delete</button></td></tr>`;
+    });
+
     clearButton.style.display = "block";
+    table.style.display = "table";
+  } else {
+    clearButton.style.display = "none";
+    table.style.display = "none";
   }
 }
 
-async function storeParamsData() {
+async function updateTable() {
   await setHostParamsData(host, queryParams);
+  await updateTabUrl(await getUrl());
+
+  renderTable();
 }
 
+// Adds a new query parameter
 async function submitParamsForm(event) {
   let key = document.getElementById("key").value;
   let value = document.getElementById("value").value;
   queryParams[key] = value;
 
-  await storeParamsData();
-  await updateTabUrl(await getUrl());
-
-  renderTable();
+  updateTable();
 
   event.preventDefault();
 }
 
+// Removes all params from current host.
 function clear() {
   queryParams = {};
-  storeParamsData();
 
-  paramsTable.innerHTML = ``;
-  clearButton.style.display = "none";
+  updateTable();
 }
 
+// Loading the stored data from local storage.
 async function loadInitialData() {
   host = await getHost();
   queryParams = await retrieveHostParamsData(host);
-  console.log(JSON.stringify(queryParams));
   renderTable();
 }
 
-let host = "";
+// Deletes a key value pair on button click.
+async function removeParam(event) {
+  if (event.target.id === "delete-button") {
+    const key = event.target.dataset.key;
+
+    delete queryParams[key];
+
+    updateTable();
+  }
+}
+
+// Stores the hostname of the currently active tab.
+let host = ""; // Set in `loadInitialData()`
 
 chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
   let url = new URL(tabs[0].url);
@@ -72,11 +76,15 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
 const form = document.getElementById("query-params-form");
 form.addEventListener("submit", submitParamsForm);
 
-const paramsTable = document.getElementById("table");
+// const table = document.getElementById("params-table");
+const table = document.getElementsByTagName("table")[0];
+const tableBody = document.getElementsByTagName("tbody")[0];
 
 const clearButton = document.getElementById("clear-button");
 clearButton.addEventListener("click", clear);
-clearButton.style.display = "none";
+
+// Add listener to table for delete buttons
+tableBody.addEventListener("click", removeParam);
 
 let queryParams = {};
 
